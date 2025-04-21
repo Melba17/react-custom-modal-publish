99cb11a48f0c692433bdc29b6a676c7b980656db
Melba17 <evabancerowski77@gmail.com>
2025-04-21 13:47:36 +0200

    commentaires divers

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,5 +1,7 @@
 import './Modal.css'
+// Importe le composant React officiel de FontAwesome pour afficher des icônes en JSX
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// Importe deux icônes spécifiques depuis la bibliothèque "free-solid" (style du pack d'icônes : solid, regular, light, duotone etc.. ) de FontAwesome (ici, croix et fusée)
 import { faTimes, faRocket } from '@fortawesome/free-solid-svg-icons'
 
 /**
@@ -14,6 +16,7 @@ function Modal({ isOpen, onClose, title, children }) {
   if (!isOpen) return null
 
   return (
+    // Modale en superposition qui couvre tout l'écran et désactive l'interaction avec l'arrière-plan
     <div className="modal-overlay" role="dialog" aria-modal="true">
       <div className="modal-content">
         <button
